553981fa6134f1f6b59d88dc917b6cb799a1da72
Ubuntu <ubuntu@ip-172-31-88-157.ec2.internal>
2019-07-01 10:39:54 +0000

    Add slideToggle hide/show on paragraph

--- js/script.js
+++ js/script.js
@@ -28,6 +28,19 @@ $(document).ready(function() {
 	$("button").mouseleave(function(){
 		$("button").removeClass("makeBorder").addClass("makeRed");
 	});
+	
+	
+	
+	$("button").click(function() {
+		$("p").slideToggle(2000);
+	});
+	
+    // hides/shows paragraphs when either button is clicked
+	$("button").click(function() {
+		$("p").hide(2000).show(2000);
+	});
+
+
 
 }); 
     
